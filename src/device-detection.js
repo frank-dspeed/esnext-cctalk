29dee611c0a48c130c2dde5aa6f8a1431ff23e0e
Frank Lemanschik <frank@lemanschik.com>
2021-06-08 17:10:25 +0200

    Add: detect

--- src/device-detection.js
+++ src/device-detection.js
@@ -28,13 +28,35 @@ const simplePollBillAcceptor = [ 40, 0, 182, 254, 33 ];
     //coindDetector(...readBufferedCredit()).then(readBufferedCreditEvents).then( coin => {
 
     //coindDetector()
-const detedDevice = [
+
+const detectedDevice = [
        246, //Request manufacturer id //Core commands
         245, //Request equipment category id //Core commands
         244, //Request product code //Core commands
         243, //Request database version //Coin Acceptor commands
         242, //Request serial number //Core Plus commands
         241, //Request software revision //Core Plus commands
-].forEach(async (cmd)=>{
-    coindDetector(cmd).then(getMessage).then(msg=> String.fromCharCode.apply(null, msg.data)).then(Debug('DETECTED'))
+]
+
+// .forEach(async (cmd)=>{
+//     coindDetector(cmd).then(getMessage).then(msg=> String.fromCharCode.apply(null, msg.data)).then(Debug('DETECTED'))
+// })
+
+
+const standardAddresses = [2,40];
+
+standardAddresses.forEach(async adr=>{
+    await getDeviceWriter(connection,adr,8)(254).then(()=>{
+        // found
+        detectedDevice.forEach(async (cmd)=>{
+            await getDeviceWriter(connection,adr,8)(cmd).then(getMessage).then(msg=> String.fromCharCode.apply(null, msg.data)).then(Debug('DETECTED'))
+        })
+    })
+
+    await getDeviceWriter(connection,adr,16).then(()=>{
+        // found
+        detectedDevice.forEach(async (cmd)=>{
+            await getDeviceWriter(connection,adr,16)(cmd).then(getMessage).then(msg=> String.fromCharCode.apply(null, msg.data)).then(Debug('DETECTED'))
+        })
+    })
 })
