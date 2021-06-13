59be07d8512ed483c371acf0c180054272626932
Frank Lemanschik <frank@lemanschik.com>
2021-06-13 18:13:24 +0200

    detect - device dev

--- modules/parse-command-reply-pairs.js
+++ modules/parse-command-reply-pairs.js
@@ -50,7 +50,8 @@ import { getDestHeaderDataFromPayloadAsObject } from './payload-helpers.js';
                     currentPromise.resolve(messageAsUint8Array);
                     return 
                 }
-
+                currentProcessingPromises.splice(0,2);    
+                
                 // throw error here is something wrong.
                 Debug('esnext-cctalk/node/connection/parser/onData/completPair/error')('!completPair')
                 Debug('esnext-cctalk/node/connection/parser/onData/completPair/error')({ currentProcessingPromise, messageAsUint8Array })
