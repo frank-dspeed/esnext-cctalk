c14e9c1ad317a9cb97affbc95fd0d4ef9d233f9f
Frank Lemanschik <frank@lemanschik.com>
2021-06-13 14:00:18 +0200

    detect - device dev

--- examples/nodejs-serialport/get-device-usage.js
+++ examples/nodejs-serialport/get-device-usage.js
@@ -10,9 +10,15 @@ detectDevices(async dev=> {
     console.log('Found', { dev })
     //if (i++ === 1) {
         // We have a perfect loop
+        const { write, info, crcMethodName, adr } = dev;
         promiseChain = promiseChain
-        .then(()=> tryPoll(dev.write).then(x=>console.log('connected:',x, { dev })) ) 
+        .then(()=> tryPoll(dev.write).then(x=>console.log('connected:',x, { info, crcMethodName, adr })) ).then(()=>process.exit())
         //
     //}
     
 })
+
+// write,
+// info,
+// crcMethodName,
+// adr,
