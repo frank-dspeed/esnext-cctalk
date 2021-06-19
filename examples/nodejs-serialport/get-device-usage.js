e45f6e5cfc46f23bbd0a9f5b05c74deaefd17cee
Frank Lemanschik <frank@lemanschik.com>
2021-06-19 16:43:33 +0200

    Iteration

--- examples/nodejs-serialport/get-device-usage.js
+++ examples/nodejs-serialport/get-device-usage.js
@@ -1,26 +1,10 @@
-import detectDevices from '../../src/device-detection.js';
-import { delayResolvePromise } from '../../modules/promises-delayed.js';
-let i = 0;
+import getDevices from '../../src/device-detection.js';
+import { getEventData,getEventsAsArrays,isValidEventPayload } from '../..//modules/parser/on-valid-event-message.js'
 
-// @ts-ignore
-const tryPoll = write => delayResolvePromise(1000).then(write(254).catch(()=>tryPoll(write)));
-let promiseChain = Promise.resolve();
-// @ts-ignore
-detectDevices(async dev=> {
-    console.log('Found', { dev })
-    //if (i++ === 1) {
-    /*    
-    // We have a perfect loop
-        const { write, info, crcMethodName, adr } = dev;
-        promiseChain = promiseChain
-        .then(()=> tryPoll(dev.write).then(x=>console.log('connected:',x, { info, crcMethodName, adr })) ).then(()=>process.exit())
-        //
-    //}
-    */
-    
-})
-
-// write,
-// info,
-// crcMethodName,
-// adr,
+const devices = await getDevices();
+for (const device of devices) {
+    await device.write(254);
+    await device.write(231, Uint8Array.from([255, 1]));
+    await device.write(228, Uint8Array.from([0xFF]));
+    // Enter Loop
+}
